ff821716e92f62097c22c600ac977ff00cf3d61c
Krus_LEGION_4070 <selfishkrus@github.com>
2024-01-26 14:16:07 +0800

    <pp> outline

--- Assets/com.krus.toonshading/RendererFeature/OutlineRendererFeature.cs
+++ Assets/com.krus.toonshading/RendererFeature/OutlineRendererFeature.cs
@@ -1,51 +1,190 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-public class OutlineRendererFeature : ScriptableRendererFeature
-{
-    class CustomRenderPass : ScriptableRenderPass
-    {
-        // This method is called before executing the render pass.
-        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
-        // When empty this render pass will render to the active camera render target.
-        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
-        // The render pipeline will ensure target setup and clearing happens in a performant manner.
-        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
-        {
-        }
+internal class OutlineRendererFeature : ScriptableRendererFeature
+{   
 
-        // Here you can implement the rendering logic.
-        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
-        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
-        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
-        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
-        {
-        }
+    //////////////
+    // Settings // 
+    //////////////
 
-        // Cleanup any allocated resources that were created during the execution of this render pass.
-        public override void OnCameraCleanup(CommandBuffer cmd)
-        {
-        }
+    [System.Serializable]
+    public class OutlineSettings 
+    {   
+        [Header("Render Pass")]
+        public Material material;
+        public RenderPassEvent renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+        public string colorTargetDestinationID = "_CamColTex";
+
+        // [Header("Depth Outline")]
+        // public bool enableDepthOutline = true;
+        // [Range(0.0f, 1.0f)]
+        // public float depthThreshold = 0.5f;
+        // [Range(0.0f, 0.03f)]
+        // public float depthThickness = 0.01f;
+        // [Range(0.0f, 1.0f)]
+        // public float depthSmoothness = 0.0f;
+        // public Color depthOutlineColor = Color.black;
+
+        // [Header("Normal Outline")]
+        // public bool enableNormalOutline = true;
+        // [Range(0.0f, 1.0f)]
+        // public float normalThreshold = 0.5f;
+        // [Range(0.0f, 0.03f)]
+        // public float normalThickness = 0.01f;
+        // [Range(0.0f, 1.0f)]
+        // public float normalSmoothness = 0.0f;
+        // public Color normalOutlineColor = Color.black;
+        
     }
 
-    CustomRenderPass m_ScriptablePass;
+    //////////////////////
+    // Renderer Feature // 
+    //////////////////////
 
-    /// <inheritdoc/>
+    public OutlineSettings settings = new OutlineSettings();
+
+    Material m_Material;
+
+    DepthOutlineRenderPass m_RenderPass = null;
+
+    
+
     public override void Create()
     {
-        m_ScriptablePass = new CustomRenderPass();
-
-        // Configures where the render pass should be injected.
-        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+        m_RenderPass = new DepthOutlineRenderPass(settings);
     }
 
-    // Here you can inject one or multiple render passes in the renderer.
-    // This method is called when setting up the renderer once per-camera.
-    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    public override void AddRenderPasses(ScriptableRenderer renderer,
+                                    ref RenderingData renderingData)
     {
-        renderer.EnqueuePass(m_ScriptablePass);
+        // if (renderingData.cameraData.camera.cameraType != CameraType.Game && renderingData.cameraData.camera.cameraType != CameraType.SceneView)
+            renderer.EnqueuePass(m_RenderPass);
+            m_RenderPass.ConfigureInput(ScriptableRenderPassInput.Color);
+            m_RenderPass.ConfigureInput(ScriptableRenderPassInput.Normal);
+    }
+
+    public override void SetupRenderPasses(ScriptableRenderer renderer,
+                                        in RenderingData renderingData)
+    {
+        // if (renderingData.cameraData.camera.cameraType != CameraType.Game && renderingData.cameraData.camera.cameraType != CameraType.SceneView)
+        // {
+            m_RenderPass.SetTarget(renderer.cameraColorTargetHandle);
+        // }
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        CoreUtils.Destroy(m_Material);
+        m_RenderPass.Dispose();
+    }
+
+    //////////////////////
+    //   Renderer Pass  // 
+    //////////////////////
+
+    internal class DepthOutlineRenderPass : ScriptableRenderPass
+    {   
+        ProfilingSampler m_profilingSampler = new ProfilingSampler("Outline");
+        Material m_material;
+        RTHandle m_cameraColorTarget;
+        RTHandle rtCustomColor, rtTempColor;
+        OutlineSettings m_settings;
+
+        public DepthOutlineRenderPass(OutlineSettings settings)
+        {   
+            m_material = settings.material;
+            this.m_settings = settings;
+            renderPassEvent = m_settings.renderPassEvent;
+        }
+
+        public void SetTarget(RTHandle colorHandle)
+        {
+            m_cameraColorTarget = colorHandle;
+        }
+
+        // Pass Data //
+
+        public void PassShaderData(Material material)
+        {
+            // if (m_settings.enableDepthOutline)
+            // {
+                // material.EnableKeyword("_DEPTH_OUTLINE");
+            // }
+            // material.SetFloat("_Depth_Threshold", m_settings.depthThreshold);
+            // material.SetFloat("_Depth_Thickness", m_settings.depthThickness);
+            // material.SetFloat("_Depth_Smoothness", m_settings.depthSmoothness);
+            // material.SetVector("_Depth_OutlineColor", m_settings.depthOutlineColor);
+
+            // if (m_settings.enableNormalOutline)
+            // {
+            //     material.EnableKeyword("_NORMAL_OUTLINE");
+            // }
+            // material.SetFloat("_Normal_Threshold", m_settings.normalThreshold);
+            // material.SetFloat("_Normal_Thickness", m_settings.normalThickness);
+            // material.SetFloat("_Normal_Smoothness", m_settings.normalSmoothness);
+            // material.SetVector("_Normal_OutlineColor", m_settings.normalOutlineColor);
+        }
+
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+        {   
+            var colorDesc = renderingData.cameraData.cameraTargetDescriptor;
+            colorDesc.depthBufferBits = 0;
+
+            // Set up temporary color buffer (for blit)
+
+            RenderingUtils.ReAllocateIfNeeded(ref rtCustomColor, colorDesc, name: "_RTCustomColor");
+            RenderingUtils.ReAllocateIfNeeded(ref rtTempColor, colorDesc, name: "_RTTempColor");
+
+            ConfigureTarget(m_cameraColorTarget);
+            ConfigureTarget(rtCustomColor);
+            ConfigureTarget(rtTempColor);
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            var cameraData = renderingData.cameraData;
+
+            if (m_material == null)
+                return;
+
+            CommandBuffer cmd = CommandBufferPool.Get();
+            using (UnityEngine.Rendering.ProfilingScope profilingScope = new UnityEngine.Rendering.ProfilingScope(cmd, m_profilingSampler))
+            {
+                PassShaderData(m_material);
+                m_material.SetTexture(m_settings.colorTargetDestinationID, m_cameraColorTarget);
+
+                RTHandle rtCamera = renderingData.cameraData.renderer.cameraColorTargetHandle;
+
+                Blitter.BlitCameraTexture(cmd, m_cameraColorTarget, rtCustomColor, m_material, 0);
+                Blitter.BlitCameraTexture(cmd, rtCustomColor, m_cameraColorTarget);
+            }
+
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+
+            CommandBufferPool.Release(cmd);
+
+        }
+
+        public override void OnCameraCleanup(CommandBuffer cmd)
+        {
+
+        }
+
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            base.FrameCleanup(cmd);
+            // rtCustomColor.Release();
+            // rtTempColor.Release();
+        }
+
+        public void Dispose()
+        {
+            rtCustomColor.Release();
+            rtTempColor.Release();
+        }
     }
 }
-
-
---
+++ Assets/com.krus.toonshading/RendererFeature/Shader_Blit_Outline.shader
@@ -0,0 +1,134 @@
+Shader "Blit/Outline"
+{
+    Properties
+    {   
+        [Toggle(_DEPTH_OUTLINE)] _Depth_Outline("Depth Outline", Float) = 1
+        _Depth_Threshold("Depth Threshold", Range(0, 1)) = 0.75
+        _Depth_Thickness("Depth Thickness", Range(0, 0.01)) = 0.01
+        _Depth_Smoothness("Depth Smoothness", Range(0, 1)) = 0.5
+        _Depth_OutlineColor("Depth Outline Color", Color) = (0, 0, 0, 0)
+
+        [Space(30)]
+
+        [Toggle(_NORMAL_OUTLINE)] _Normal_Outline("Normal Outline", Float) = 1
+        _Normal_Threshold("Normal Threshold", Range(0, 1)) = 0.75
+        _Normal_Thickness("Normal Thickness", Range(0, 0.01)) = 0.01
+        _Normal_Smoothness("Normal Smoothness", Range(0, 1)) = 0.5
+        _Normal_OutlineColor("Normal Outline Color", Color) = (0, 0, 0, 0)
+        _Normal_Bias("Normal Bias", Range(0, 1)) = 0.1
+    }
+
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" "RenderPipeline" = "UniversalPipeline"}
+        LOD 100
+        ZWrite Off 
+        Cull Off
+        Pass
+        {
+            Name "DepthOutlinePass"
+
+            HLSLPROGRAM
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareNormalsTexture.hlsl"
+
+            // The Blit.hlsl file provides the vertex shader (Vert),
+            // input structure (Attributes) and output strucutre (Varyings)
+            #include "Packages/com.unity.render-pipelines.core/Runtime/Utilities/Blit.hlsl"
+
+            #pragma vertex Vert
+            #pragma fragment frag
+            #pragma multi_compile _ _DEPTH_OUTLINE
+            #pragma multi_compile _ _NORMAL_OUTLINE
+
+            TEXTURE2D_X(_CamColTex);        SAMPLER(sampler_CamColTex);
+
+            float _Depth_Threshold;
+            float _Depth_Thickness;
+            float _Depth_Smoothness;
+            float3 _Depth_OutlineColor;
+
+            float _Normal_Threshold;
+            float _Normal_Thickness;
+            float _Normal_Smoothness;
+            float3 _Normal_OutlineColor;
+            float _Normal_Bias;
+
+            half4 frag (Varyings input) : SV_Target
+            {
+                UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+                float4 camColTex = SAMPLE_TEXTURE2D_X(_CamColTex, sampler_CamColTex, input.texcoord);
+                float3 outCol = camColTex.rgb;
+
+                // sobel operator 
+                const half Gx[9] = {
+                    -1, 0, 1,
+                    -2, 0, 2,
+                    -1, 0, 1
+                };
+                const half Gy[9] = {
+                    1, 2, 1,
+                    0, 0, 0,
+                    -1, -2, -1
+                };
+
+                float depth = SampleSceneDepth(input.texcoord.xy).r;
+                depth = Linear01Depth(depth, _ZBufferParams);
+
+                #ifdef _DEPTH_OUTLINE
+
+                    // depth comparison
+                    float edgeX_depth = 0;
+                    float edgeY_depth = 0;
+
+                    for (int i = 0; i < 9; i++) 
+                    {
+                        half2 offset = half2(i % 3, i / 3) - half2(1, 1);
+                        half depthOffset = SampleSceneDepth(input.texcoord.xy + offset * _Depth_Thickness).r;
+                        depthOffset = Linear01Depth(depthOffset, _ZBufferParams);
+                        edgeX_depth += depthOffset * Gx[i];
+                        edgeY_depth += depthOffset * Gy[i];
+                    };
+
+                    edgeX_depth /= 9;
+                    edgeY_depth /= 9;
+
+                    float edge_depth = (1 - sqrt(edgeX_depth * edgeX_depth + edgeY_depth * edgeY_depth));
+                    edge_depth = smoothstep(_Depth_Threshold-_Depth_Smoothness, _Depth_Threshold+_Depth_Smoothness, edge_depth);
+
+                    outCol = lerp(_Depth_OutlineColor, outCol, edge_depth);
+
+                #endif
+
+                #ifdef _NORMAL_OUTLINE
+                
+                    // normal comparison
+                    float edgeX_normal = 0;
+                    float edgeY_normal = 0;
+                    
+                    for (uint j = 0; j < 9; j++) 
+                    {
+                        half2 offset = half2(j % 3, j / 3) - half2(1, 1);
+                        half3 normalOffset = SampleSceneNormals(input.texcoord.xy + offset * _Normal_Thickness).rgb;
+                        edgeX_normal += normalOffset * Gx[j];
+                        edgeY_normal += normalOffset * Gy[j];
+                    };
+
+                    edgeX_normal /= 9;
+                    edgeY_normal /= 9;
+
+                    float edge_normal = (1 - sqrt(edgeX_normal * edgeX_normal + edgeY_normal * edgeY_normal));
+                    edge_normal = smoothstep(_Normal_Threshold-_Normal_Smoothness, _Normal_Threshold+_Normal_Smoothness, edge_normal+_Normal_Bias);
+
+                    outCol = lerp(_Normal_OutlineColor, outCol, edge_normal);
+
+                #endif
+                
+                return float4(outCol, camColTex.a);
+            }
+            ENDHLSL
+        }
+    }
+}
