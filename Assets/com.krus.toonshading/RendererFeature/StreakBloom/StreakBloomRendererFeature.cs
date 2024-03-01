4c07dcff6192096c996ffd674b568c13e21e4fd7
Krus_LEGION_4070 <selfishkrus@github.com>
2024-03-01 22:10:19 +0800

    WIP

--- Assets/com.krus.toonshading/RendererFeature/StreakBloom/StreakBloomRendererFeature.cs
+++ Assets/com.krus.toonshading/RendererFeature/StreakBloom/StreakBloomRendererFeature.cs
@@ -60,7 +60,6 @@ internal class StreakBloomRendererFeature : ScriptableRendererFeature
 
     protected override void Dispose(bool disposing)
     {
-        CoreUtils.Destroy(m_Material);
         m_RenderPass.Dispose();
     }
 
@@ -70,11 +69,13 @@ internal class StreakBloomRendererFeature : ScriptableRendererFeature
 
     internal class StreakBloomRenderPass : ScriptableRenderPass
     {   
-        ProfilingSampler m_profilingSampler = new ProfilingSampler("Outline");
+        ProfilingSampler m_profilingSampler = new ProfilingSampler("StreakBloom");
         Material m_material;
         RTHandle m_cameraColorTarget;
-        RTHandle rtCustomColor, rtTempColor;
+        RTHandle rtTempColor0, rtTempColor1;
         StreakBloomSettings m_settings;
+
+        StreakPyramid pyramid;
 
         public StreakBloomRenderPass(StreakBloomSettings settings)
         {   
@@ -108,57 +109,63 @@ internal class StreakBloomRendererFeature : ScriptableRendererFeature
 
             // Set up temporary color buffer (for blit)
 
-            RenderingUtils.ReAllocateIfNeeded(ref rtCustomColor, colorDesc, name: "_RTCustomColor");
-            RenderingUtils.ReAllocateIfNeeded(ref rtTempColor, colorDesc, name: "_RTTempColor");
-
-            ConfigureTarget(m_cameraColorTarget);
-            ConfigureTarget(rtCustomColor);
-            ConfigureTarget(rtTempColor);
+            RenderingUtils.ReAllocateIfNeeded(ref rtTempColor0, colorDesc, name: "_RTTempColor0");
+            RenderingUtils.ReAllocateIfNeeded(ref rtTempColor1, colorDesc, name: "_RTTempColor1");
 
             m_material = m_settings.material;
+
+            ConfigureTarget(m_cameraColorTarget);
+            ConfigureTarget(rtTempColor0);
+            ConfigureTarget(rtTempColor1);
+
+            // Configure render target for _pyramids
+            pyramid = GetPyramid(renderingData.cameraData.camera);
+            // pyramid.ConfigureTarget(this);
+            
         }
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
-            var cameraData = renderingData.cameraData;
-
             if (m_material == null)
                 return;
 
             if (m_cameraColorTarget.rt == null)
                 return;
 
-            PassShaderData(m_material);
-
             CommandBuffer cmd = CommandBufferPool.Get();
             using (UnityEngine.Rendering.ProfilingScope profilingScope = new UnityEngine.Rendering.ProfilingScope(cmd, m_profilingSampler))
             {   
-                // Prefilter
-                m_material.SetTexture("_CamColTex", m_cameraColorTarget);
-                Blitter.BlitCameraTexture(cmd, m_cameraColorTarget, rtCustomColor, m_material, 0);
 
-                Blitter.BlitCameraTexture(cmd, rtCustomColor, m_cameraColorTarget);
+                m_material.SetTexture("_CamColTex", m_cameraColorTarget);
+                PassShaderData(m_material);
+                // pyramid = GetPyramid(renderingData.cameraData.camera);
+
+                // Prefilter
+                // CameraColorTarget -> Prefilter -> MIP 0
+                // Blitter.BlitCameraTexture(cmd, m_cameraColorTarget, pyramid[0].down, m_material, 0);
+                // Blitter.BlitCameraTexture(cmd, pyramid[0].down, m_cameraColorTarget);
+                
+                Blitter.BlitCameraTexture(cmd, m_cameraColorTarget, rtTempColor0, m_material, 0);
+                Blitter.BlitCameraTexture(cmd, rtTempColor0, m_cameraColorTarget);
             }
 
             context.ExecuteCommandBuffer(cmd);
             cmd.Clear();
 
             CommandBufferPool.Release(cmd);
-
         }
 
         public override void OnCameraCleanup(CommandBuffer cmd)
         {
-            base.OnCameraCleanup(cmd);
-            cmd.ReleaseTemporaryRT(rtCustomColor.GetInstanceID());
-            cmd.ReleaseTemporaryRT(rtTempColor.GetInstanceID());
-            cmd.ReleaseTemporaryRT(m_cameraColorTarget.GetInstanceID());
+        
         }
 
         public void Dispose()
-        {
-            RTHandles.Release(rtCustomColor);
-            RTHandles.Release(rtTempColor);
+        {   
+            if (rtTempColor0 != null) RTHandles.Release(rtTempColor0);
+            if (rtTempColor1 != null) RTHandles.Release(rtTempColor1);
+
+            foreach (var pyramid in _pyramids.Values) pyramid.Release();
         }
 
         // Pyramid to store images
@@ -167,19 +174,8 @@ internal class StreakBloomRendererFeature : ScriptableRendererFeature
         StreakPyramid GetPyramid(Camera camera)
         {
             StreakPyramid candid;
-            var cameraID = camera.GetInstanceID();
-
-            if (_pyramids.TryGetValue(cameraID, out candid))
-            {
-                // Reallocate the RTs when the screen size was changed.
-                if (!candid.CheckSize(camera)) candid.Reallocate(camera);
-            }
-            else
-            {
-                // No one found: Allocate a new pyramid.
-                _pyramids[cameraID] = candid = new StreakPyramid(camera);
-            }
-
+            candid = new StreakPyramid(camera);
+            
             return candid;
         }
     }
@@ -188,7 +184,7 @@ internal class StreakBloomRendererFeature : ScriptableRendererFeature
 
     sealed class StreakPyramid
     {
-        public const int MaxMipLevel = 16;
+        public const int MaxMipLevel = 8;
 
         int _baseWidth, _baseHeight;
         readonly (RTHandle down, RTHandle up) [] _mips = new (RTHandle, RTHandle) [MaxMipLevel];
@@ -205,13 +201,22 @@ internal class StreakBloomRendererFeature : ScriptableRendererFeature
 
         public bool CheckSize(Camera camera)
         {
-            return _baseWidth == camera.pixelWidth && _baseHeight == camera.pixelHeight;
+            return _baseWidth == camera.scaledPixelWidth && _baseHeight == camera.scaledPixelHeight;
         }
 
         public void Reallocate(Camera camera)
         {
             Release();
             Allocate(camera);
+        }
+
+        public void ConfigureTarget(ScriptableRenderPass renderPass)
+        {
+            foreach (var mip in _mips)
+            {
+                if (mip.down != null) renderPass.ConfigureTarget(mip.down);
+                if (mip.up   != null) renderPass.ConfigureTarget(mip.up);
+            }
         }
 
         public void Release()
@@ -225,11 +230,11 @@ internal class StreakBloomRendererFeature : ScriptableRendererFeature
 
         void Allocate(Camera camera)
         {
-            _baseWidth = camera.pixelWidth;
-            _baseHeight = camera.pixelHeight;
+            _baseWidth = camera.scaledPixelWidth;
+            _baseHeight = camera.scaledPixelHeight;
 
-            var width = _baseWidth;
-            var height = _baseHeight / 2;
+            var width = _baseWidth / 2;
+            var height = _baseHeight / 4;
 
             const GraphicsFormat RTFormat = GraphicsFormat.R16G16B16A16_SFloat;
 
